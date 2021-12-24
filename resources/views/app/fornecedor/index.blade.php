63b205b820be7237642feff4361e2095f0b10c29
Luiz Amaral <amaralluiz@DESKTOP-65RP3PA.localdomain>
2021-12-24 10:53:02 -0300

    Aprendendo utilizacao de if na sintaxe do blade

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,24 @@
+<h3>Fornecedor</h3>
+
+
+@php 
+/* 
+    if() {
+
+    } elseif (condition) {
+        # code...
+    }
+    else {
+        # code...
+    }
+ */
+@endphp
+
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif
