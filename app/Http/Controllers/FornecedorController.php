3c2b5f32de0d8c4597083088d62487cec4e969aa
Luiz Amaral <amaralluiz@DESKTOP-65RP3PA.localdomain>
2021-12-24 11:11:09 -0300

    Aprendendo uso do unless na sintaxe blade

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,7 +7,9 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index() {
-        $fornecedores = [];
+        $fornecedores = [
+            0 => ['nome' => 'Fonecedor 1', 'status' => 'N']
+        ];
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
